6cfe290ddc0d9b1369dde25d4f6a0c9e0db2c614
bick4ord <bick4ord@gmail.com>
2021-06-11 18:26:22 -0400

    first commit

---
+++ MaficPipes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+
+namespace MaficPipes
+{
+    public static class MaficPipesPatches
+    {
+        [HarmonyPatch(typeof(ElementLoader))]
+        [HarmonyPatch("FinaliseElementsTable")]
+        public static class ElementLoader_LoadUserElementData_Patch
+        {
+            public static void Postfix()
+            {
+                var mafic = ElementLoader.FindElementByHash(SimHashes.MaficRock);
+                var tags = new List<Tag>(mafic.oreTags) { new Tag("Plumbable") };
+                mafic.oreTags = tags.ToArray();
+            }
+        }
+    }
+
+}
